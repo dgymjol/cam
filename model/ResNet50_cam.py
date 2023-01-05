2fe57abd6dea98a37ed4b09c53565af685510b33
sjpark <dgymjol@yonsei.ac.kr>
2023-01-05 11:24:02 +0900

    model modifiy

--- model/ResNet50_cam.py
+++ model/ResNet50_cam.py
@@ -14,69 +14,55 @@ class ResNet50_cam(nn.Module):
 
         self.num_classes = num_classes
 
-        ## conv1 : 7x7 64 stride 2
-        self.conv1 = nn.Conv2d(3, 64, 7, stride=2, padding=3, bias=False)
-        self.bn = nn.BatchNorm2d(64)
-        self.relu = nn.ReLU(inplace=True)
+        # conv0 : 7x7 64 stride 2
+        self.layer0 = nn.Sequential(
+            nn.Conv2d(3, 64, 3, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True)
+        )
 
-        ## 3x3 max pool stride 2
+        # 3x3 max pool stride 2
         self.mp = nn.MaxPool2d(3, stride=2, padding=1)
 
-        residual_blocks = []
-        ## conv2 : bottleneck 64, out 256, num 3
-        residual_blocks.append(Residual_blocks(in_channel=64, bottleneck_channel=64, out_channel=256, block_num=3, first_layer=True))
+        # conv1 : bottleneck 64, out 256, num 3
+        self.layer1 = self._make_layer(in_channel=64, bottleneck_channel=64, out_channel=256, block_num=3, stride=1)
 
-        # conv3 : bottleneck 128, out 512, num 4
-        residual_blocks.append(Residual_blocks(in_channel=256, bottleneck_channel=128, out_channel=512, block_num=4, first_layer=False))
+        # conv2 : bottleneck 128, out 512, num 4
+        self.layer2 = self._make_layer(in_channel=256, bottleneck_channel=128, out_channel=512, block_num=4, stride=2)
 
-        ## conv4 : bottleneck 256, out 1024, num 6
-        residual_blocks.append(Residual_blocks(in_channel=512, bottleneck_channel=256, out_channel=1024, block_num=6, first_layer=False))
+        # conv3 : bottleneck 256, out 1024, num 6
+        self.layer3 = self._make_layer(in_channel=512, bottleneck_channel=256, out_channel=1024, block_num=6, stride=2)
 
-        # conv5 : bottleneck 512, out 2048, num 3  => Remove to increse map resolution
-        # residual_blocks.append(Residual_blocks(in_channel=1024, bottleneck_channel=512, out_channel=2048, block_num=3, first_layer=False))
-
-        self.rbs = nn.Sequential(*residual_blocks)
+        # conv4 : bottleneck 512, out 2048, num 3 => Remove to increse map resolution
+        # self.layer4 = self._make_layer(in_channel=1024, bottleneck_channel=512, out_channel=2048, block_num=3, stride=2)
 
         self.final_conv = nn.Conv2d(1024, 1024, 3, stride=1, padding=1, bias=False)
-
         self.w = nn.Conv2d(1024, self.num_classes, 1, bias = False) # conv weight => (num_classes, k, 1, 1)
 
     def forward(self, x):
-        x = self.mp(self.relu(self.bn(self.conv1(x))))
-        x = self.rbs(x)
-        x = self.final_conv(x)
+        x = self.mp(self.layer0(x))
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        # x = self.layer4(x)
 
         GAP = self.w(x)
         S_c = torch.sum(GAP.reshape(GAP.shape[0], self.num_classes, -1), 2)
 
-        return GAP, S_c
+        return S_c, GAP
 
-
-class Residual_blocks(nn.Module):
-    def __init__(self, 
-                 in_channel, 
-                 bottleneck_channel, 
-                 out_channel, 
-                 block_num,
-                 first_layer):
-        super().__init__()
+    def _make_layer(self, in_channel, bottleneck_channel, out_channel, block_num, stride):
 
         blocks = []
-        if first_layer:
-            blocks.append(Residual_block(in_channel, bottleneck_channel, out_channel, 1))
-        else:
-            blocks.append(Residual_block(in_channel, bottleneck_channel, out_channel, 2))
-
+        blocks.append(Bottleneck_block(in_channel, bottleneck_channel, out_channel, stride))
         for i in range(block_num - 1):
-            blocks.append(Residual_block(out_channel, bottleneck_channel, out_channel, 1))
+            blocks.append(Bottleneck_block(out_channel, bottleneck_channel, out_channel, 1))
 
-        self.blocks = nn.Sequential(*blocks)
-
-    def forward(self, x):
-        return self.blocks(x)
+        return nn.Sequential(*blocks)
 
 
-class Residual_block(nn.Module):
+
+class Bottleneck_block(nn.Module):
     def __init__(self, 
                  in_channel, 
                  bottleneck_channel, 
@@ -84,24 +70,20 @@ class Residual_block(nn.Module):
                  stride):
         super().__init__()
 
-        self.conv1 = nn.Conv2d(in_channel, bottleneck_channel, 1, bias=False)
-
-        self.bn1 = nn.BatchNorm2d(bottleneck_channel)
-        self.relu1 = nn.ReLU(inplace=True)
-
-        self.conv2 = nn.Conv2d(bottleneck_channel, bottleneck_channel, 3,
-                               padding=1, stride=stride, bias=False)
-
-        self.bn2 = nn.BatchNorm2d(bottleneck_channel)
-        self.relu2 = nn.ReLU(inplace=True)
-
-        self.conv3 = nn.Conv2d(bottleneck_channel, out_channel, 1, bias=False)
-        self.bn3 = nn.BatchNorm2d(out_channel)
+        self.convolutions = nn.Sequential(
+            nn.Conv2d(in_channel, bottleneck_channel, 1, bias=False),
+            nn.BatchNorm2d(bottleneck_channel),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(bottleneck_channel, bottleneck_channel, 3, padding=1, stride=stride, bias=False),
+            nn.BatchNorm2d(bottleneck_channel),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(bottleneck_channel, out_channel, 1, bias=False),
+            nn.BatchNorm2d(out_channel)
+        )
 
         if stride == 1:
             if in_channel == out_channel:
-                self.shortcut = nn.Sequential(nn.Identity(),
-                                              nn.BatchNorm2d(out_channel))
+                self.shortcut = nn.Identity()
             else:
                 self.shortcut = nn.Sequential(nn.Conv2d(in_channel, out_channel, 1, bias=False),
                                               nn.BatchNorm2d(out_channel))
@@ -111,12 +93,8 @@ class Residual_block(nn.Module):
         else :
             print(stride)
             assert False, "stride error"
+
+        self.relu = nn.ReLU(inplace=True)
     
     def forward(self, x):
-        out = self.relu1(self.bn1(self.conv1(x)))
-        out = self.relu2(self.bn2(self.conv2(out)))
-        out = self.bn3(self.conv3(out))
-
-        sc = self.shortcut(x)
-
-        return out+sc
+        return self.relu(self.convolutions(x) + self.shortcut(x))
